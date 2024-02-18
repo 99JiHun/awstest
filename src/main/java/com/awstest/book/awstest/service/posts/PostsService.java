1f5b3850c2e0e6cf13f49a28cbc250478b1429d2
JiHun Choe <94276325+99JiHun@users.noreply.github.com>
2024-02-18 23:33:37 +0900

    ch4,5(gitignore) git 등록

--- src/main/java/com/awstest/book/awstest/service/posts/PostsService.java
+++ src/main/java/com/awstest/book/awstest/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.awstest.book.awstest.service.posts;
 
 import com.awstest.book.awstest.domain.posts.Posts;
 import com.awstest.book.awstest.domain.posts.PostsRepository;
+import com.awstest.book.awstest.web.dto.PostsListResponseDto;
 import com.awstest.book.awstest.web.dto.PostsResponseDto;
 import com.awstest.book.awstest.web.dto.PostsSaveRequestDto;
 import com.awstest.book.awstest.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,18 @@ public class PostsService {
                 .orElseThrow(() ->
                         new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당게시글 없음. id="+id));
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/awstest/book/awstest/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.awstest.book.awstest.web.dto;
+
+import com.awstest.book.awstest.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
